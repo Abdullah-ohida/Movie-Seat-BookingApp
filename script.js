3d0aa154619358f8cc81fe9b825bbf56d3cfc93f
Abdullah Ismail <ohida2001@gmail.com>
2021-02-23 18:28:53 +0100

    added local storagefunctionality

--- script.js
+++ script.js
@@ -14,13 +14,28 @@ const updateSelectedCount = ()=>{
     const selectedSeats = document.querySelectorAll(".row .selected");
     const selectedSeatCount = selectedSeats.length;
 
+    // copy selected seats into arr
+    // Map through arr
+    // Return a new array indexes
+
+    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+
+
     count.textContent = selectedSeatCount;
     total.textContent = +selectedSeatCount * ticketPrice;
+}
+
+// Set selected movie index and price
+const setMovieData = (movieIndex, moviePrice)=>{
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
 }
 
 // Movie Select event
 movieSelect.addEventListener("change", e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectIndex, e.target.value);
     updateSelectedCount();
 })
 
